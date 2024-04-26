560def0804ae9c26ceb065227184ca9279e89283
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-26 17:31:36 +0100

    Fix: 2d output port in verilog to 1d
    - Flattened the 2d array into 1d as Verilog doesn't accept a 2d ports.

--- Chess/ChessLayoutMatrix.v
+++ Chess/ChessLayoutMatrix.v
@@ -1,4 +1,8 @@
-module ChessLayoutMatrix (
+module ChessLayoutMatrix #(
+	parameter CHESS_SQUARES	=	64,
+	parameter SQUARE_WIDTH  =  4,
+	parameter MATRIX_WIDTH = CHESS_SQUARES * SQUARE_WIDTH
+) (
 
    /* INPUTS */
     input         KeyLeft,
@@ -8,8 +12,7 @@ module ChessLayoutMatrix (
 	 input         resetApp,
     
    /* OUTPUTS */
-    output	[3:0]	Matrix [63:0],
-); 
-
+    output reg [MATRIX_WIDTH - 1:0]	Matrix
+);
 
 endmodule
