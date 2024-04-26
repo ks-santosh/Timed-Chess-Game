43f42f11acd3b07af39cffa23cc667ceb37f38ed
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-26 17:35:16 +0100

    Feat: initialize the layout matrix
    - Chess layout matrix is initialized from hex mif file.
    - Sensitivity list is created to control the update of the matrix output.

--- Chess/ChessLayoutMatrix.v
+++ Chess/ChessLayoutMatrix.v
@@ -15,4 +15,19 @@ module ChessLayoutMatrix #(
     output reg [MATRIX_WIDTH - 1:0]	Matrix
 );
 
+reg [MATRIX_WIDTH - 1:0] InitMatrix [0:0];
+
+initial begin
+	$readmemh("MemInitFiles/ChessLayoutMatrix.hex", InitMatrix);
+	
+end 
+
+always @ (posedge KeyLeft or posedge KeyUp or posedge KeyDown or posedge KeyRight or posedge resetApp) begin
+    if (resetApp) begin
+		Matrix <= InitMatrix[0];
+    end else begin
+		Matrix <= InitMatrix[0];
+	 end
+end
+
 endmodule
