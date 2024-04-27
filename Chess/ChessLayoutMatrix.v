38533e131eacb8ddfe460aba6406784e13c2dd88
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-27 23:08:22 +0100

    Refact: flatten and pass the column layout array
    - Using the layout array to store layout.
    - Sending the layout information to output after flattening the array into bit array.

--- Chess/ChessLayoutMatrix.v
+++ Chess/ChessLayoutMatrix.v
@@ -1,6 +1,6 @@
 module ChessLayoutMatrix #(
 	parameter CHESS_SQUARES	=	64,
-	parameter SQUARE_WIDTH  =  4,
+	parameter SQUARE_WIDTH  =  8,
 	parameter MATRIX_WIDTH = CHESS_SQUARES * SQUARE_WIDTH
 ) (
 
@@ -12,21 +12,31 @@ module ChessLayoutMatrix #(
 	 input         resetApp,
     
    /* OUTPUTS */
-    output reg [MATRIX_WIDTH - 1:0]	Matrix
+    output reg [MATRIX_WIDTH - 1:0]	Layout
 );
 
-reg [MATRIX_WIDTH - 1:0] InitMatrix [0:0];
+reg [SQUARE_WIDTH - 1:0] LayoutMatrix [0:CHESS_SQUARES - 1];
+reg [MATRIX_WIDTH - 1:0] InitLayout;
+reg [MATRIX_WIDTH - 1:0] FlatLayout;
+integer i;
+
 
 initial begin
-	$readmemh("MemInitFiles/ChessLayoutMatrix.hex", InitMatrix);
-	
+	$readmemh("MemInitFiles/ChessLayoutMatrix.hex", LayoutMatrix);
+	for(i = 0; i < CHESS_SQUARES; i = i + 1) begin
+		InitLayout[i*SQUARE_WIDTH +: SQUARE_WIDTH] = LayoutMatrix[i];
+	end
 end 
+
 
 always @ (posedge KeyLeft or posedge KeyUp or posedge KeyDown or posedge KeyRight or posedge resetApp) begin
     if (resetApp) begin
-		Matrix <= InitMatrix[0];
+		Layout <= InitLayout;
     end else begin
-		Matrix <= InitMatrix[0];
+		for(i = 0; i < CHESS_SQUARES; i = i + 1) begin
+			FlatLayout[i*SQUARE_WIDTH +: SQUARE_WIDTH] = LayoutMatrix[i];
+		end		
+		Layout <= FlatLayout;
 	 end
 end
 
