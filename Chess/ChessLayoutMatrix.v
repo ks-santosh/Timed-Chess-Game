7fe7e9eaf77d92bbc59ff33da56ac2196dd46b0e
ks-santosh <159156207+SoulSantosh@users.noreply.github.com>
2024-04-28 02:19:47 +0100

    Feat: move the selection left on button press
    - Added logic that moves the selected square to the left when left key is pressed.

--- Chess/ChessLayoutMatrix.v
+++ Chess/ChessLayoutMatrix.v
@@ -5,6 +5,7 @@ module ChessLayoutMatrix #(
 ) (
 
    /* INPUTS */
+	 input			clock,
     input         KeyLeft,
     input         KeyUp,
     input         KeyDown,
@@ -28,14 +29,34 @@ initial begin
 	end
 end 
 
+reg [2:0] SelectSquareX; 
+reg [2:0] SelectSquareY;
+reg [5:0] SelectSquareIdx;
+
+localparam ON = 1'b1;
+localparam OFF = 1'b0;
 
 always @ (posedge KeyLeft or posedge KeyUp or posedge KeyDown or posedge KeyRight or posedge resetApp) begin
     if (resetApp) begin
 		Layout <= InitLayout;
+		SelectSquareX = 3'd2;
+		SelectSquareY = 3'd3;
+		SelectSquareIdx = SelectSquareY*8 + SelectSquareX;
+		LayoutMatrix[SelectSquareIdx][7:4] = 4'd1;
+		
     end else begin
+		
+		if(KeyLeft == ON) begin
+			LayoutMatrix[SelectSquareIdx][7:4] = 4'd0;
+			SelectSquareX = SelectSquareX - 1;
+			SelectSquareIdx = SelectSquareY*8 + SelectSquareX;
+			LayoutMatrix[SelectSquareIdx][7:4] = 4'd1;
+		end
+		
 		for(i = 0; i < CHESS_SQUARES; i = i + 1) begin
 			FlatLayout[i*SQUARE_WIDTH +: SQUARE_WIDTH] = LayoutMatrix[i];
 		end		
+
 		Layout <= FlatLayout;
 	 end
 end
