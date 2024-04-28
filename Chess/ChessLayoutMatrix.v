37d5ef54fe6edd9497c2460ba98a4b4dee531459
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-04-29 00:51:33 +0100

    Feat: lock the white chess piece to play
    - Logic to select the white chess piece to be moved or played.
    - After the selection is made the chess piece can be locked to be played by using the LockSwitch.
    - The locked piece square is outlined with a different colour.
    - After that with the keys user can select the target square where the piece has to be moved.

--- Chess/ChessLayoutMatrix.v
+++ Chess/ChessLayoutMatrix.v
@@ -45,9 +45,11 @@ reg [2:0] SelectSquareX;
 reg [2:0] SelectSquareY;
 reg [5:0] SelectSquareIdx;
 
-reg [2:0] LockSquareX;
-reg [2:0] LockSquareY;
-reg [2:0] LockSquareIdx; 
+reg [5:0] LockSquareIdx; 
+reg LockFlag;
+reg Player;
+localparam WHITE_PLAYER = 1'b1;
+localparam BLACK_PLAYER = 1'b0;
 
 localparam ON = 1'b0;
 localparam OFF = 1'b1;
@@ -58,14 +60,16 @@ always @ (posedge OutClock or posedge resetApp) begin
 		SelectSquareX = 3'd2;
 		SelectSquareY = 3'd3;
 		SelectSquareIdx = SelectSquareY*8 + SelectSquareX;
+		LockSquareIdx = SelectSquareIdx;
 
-		LockSquareX = 3'd0;
-		LockSquareY = 3'd0;
-
-		LayoutMatrix[SelectSquareIdx][7:4] = 4'd1;
+		Player = 1'b1; // 1 - White 0 - Black player
+		LockFlag = 1'b0;
 		
+		LayoutMatrix[SelectSquareIdx][4] = 1'b1;		
     end else begin
-		LayoutMatrix[SelectSquareIdx][7:4] = 4'd0;
+		
+		LayoutMatrix[SelectSquareIdx][4] = 1'b0;
+		LayoutMatrix[SelectSquareIdx][6] = 1'b0;
 		
 		if(KeyLeft == ON) begin
 			SelectSquareX = SelectSquareX - 1;
@@ -78,13 +82,31 @@ always @ (posedge OutClock or posedge resetApp) begin
 		end
 		
 		SelectSquareIdx = SelectSquareY*8 + SelectSquareX;
-		LayoutMatrix[SelectSquareIdx][7:4] = 4'd1;
+		LayoutMatrix[SelectSquareIdx][4] = 1'b1;
+		if(LockFlag) begin
+			LayoutMatrix[SelectSquareIdx][6] = 1'b1;
+		end
+				
+		
+		if((LockSwitch) && (!LockFlag)) begin
+			if((Player == WHITE_PLAYER) && (LayoutMatrix[SelectSquareIdx][3]))begin
+				LockSquareIdx = SelectSquareIdx;
+				LayoutMatrix[LockSquareIdx][5] = 1'b1;
+				LockFlag = 1'b1;
+			end
+		end
+		
+		if(!LockSwitch) begin
+			LayoutMatrix[LockSquareIdx][5] = 1'b0;
+			LockFlag = 1'b0;
+		end
 		
 		for(i = 0; i < CHESS_SQUARES; i = i + 1) begin
 			FlatLayout[i*SQUARE_WIDTH +: SQUARE_WIDTH] = LayoutMatrix[i];
 		end		
 
 		Layout <= FlatLayout;
+
 	 end
 end
 
