48eb9606f938d01c477c023a301dc68f7485ec58
YuehanYou <ml22y6y@leeds.ac.uk>
2024-05-01 15:56:48 +0100

    Feat: countdown from given timer value
    - The module logic starts countdown from the moment when flag is 1.
    - Countdown is converted to seconds and minutes to be displayed in 7 segment
    - On reset it will display the starting timer value

--- Chess/CountdownTimer.v
+++ Chess/CountdownTimer.v
@@ -1,6 +1,7 @@
 module CountdownTimer #( 
 	parameter MINUTES = 5;
 	parameter SECONDS = 0;
+	parameter TOTAL_SECONDS = MINUTES*60 + SECONDS;
 )(
     input clock,  // Clock input
     input reset,  // Asynchronous reset input
@@ -12,11 +13,36 @@ module CountdownTimer #(
 
 ClockFrequencyDivider #(
 	.OUTPUT_FREQUENCY(1)
-) ClockFrequencyDivider (
+) ClockSeconds (
 
-    .InClock(clock),
+   .InClock(clock),
 	.reset(reset),
-    .OutClock(OutClock)
+   .OutClock(OutClock)
 ); 
+	
+reg [9:0] SecondsLeft;
+reg [6:0] Minutes;
+reg [6:0] Seconds;
 
+    always @(posedge OutClock or posedge reset) begin
+        if (reset) begin
+            // Asynchronous reset logic
+				SecondsLeft <= TOTAL_SECONDS;
+            Minutes <= MINUTES;  // Reset minutes
+            Seconds <= SECONDS;  // Reset seconds
+				SegMins <= MINUTES;
+				SegSecTens <= SECONDS/10;
+				SegSecUnits <= SECONDS % 10;
+        end else if (flag) begin
+				// Countdown logic when flag is high
+				SecondsLeft <= SecondsLeft - 1;
+				Minutes <= SecondsLeft/60;
+				Seconds <= SecondsLeft - (Minutes * 60);
+				
+				SegMins <= Minutes;
+				SegSecTens <= Seconds/10;
+				SegSecUnits <= Seconds % 10;
+		  end
+    end
+	 
 endmodule
