b1466484b405f2fd764d72d320b73d986d6f3ff5
ks-santosh <164272377+ks-santosh@users.noreply.github.com>
2024-05-01 15:28:44 +0100

    Add: logic to count every second
    - Instantiated ClockFrequencyDivider module that turns the clock frequency to 1Hz
    - Added parameters to set the minutes and seconds of the countdown timer

--- Chess/CountdownTimer.v
+++ Chess/CountdownTimer.v
@@ -1,11 +1,22 @@
-module CountdownTimer(
-    input clk,  // Clock input
+module CountdownTimer #( 
+	parameter MINUTES = 5;
+	parameter SECONDS = 0;
+)(
+    input clock,  // Clock input
     input reset,  // Asynchronous reset input
     input flag,  // Start/pause control signal
-    output [6:0] seg_min,  // Seven-segment display output for minutes
-    output [6:0] seg_sec_tens,  // Seven-segment display output for tens of seconds
-    output [6:0] seg_sec_units  // Seven-segment display output for units of seconds
+    output [6:0] SegMins,  // Seven-segment display output for minutes
+    output [6:0] SegSecTens,  // Seven-segment display output for tens of seconds
+    output [6:0] SegSecUnits  // Seven-segment display output for units of seconds
 );
 
+ClockFrequencyDivider #(
+	.OUTPUT_FREQUENCY(1)
+) ClockFrequencyDivider (
+
+    .InClock(clock),
+	.reset(reset),
+    .OutClock(OutClock)
+); 
 
 endmodule
